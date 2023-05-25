e039dc8aaa53ec60ddb98893477fec2a9f06f44b
sumaiya81 <134583711+sumaiya81@users.noreply.github.com>
2023-05-25 12:15:41 +0530

    Add files via upload

---
+++ hight,weight(sumaiya5).py
@@ -0,0 +1,11 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/AL AZHAR/AppData/Local/Programs/Python/Python311/weight,hight(sumaiya5).py
+Hello, World!
+what is your name? Sumaiya
+Hello  Sumaiya
+>>> weight=print(36)
+36
+>>> hight=print(146)
+146
